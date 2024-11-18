060a38e05f20926321d47cf69ea1e9d05efe9dbb
guddu-paul <guddu.paul@dotsquares.com>
2024-11-18 20:21:00 +0530

    my initial commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { Provider } from './components/ui/provider.jsx'
+import { ColorModeProvider } from './components/ui/color-mode.jsx'
+import { defaultSystem } from '@chakra-ui/react'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider theme = {defaultSystem}>
+      <ColorModeProvider>
+        <App />
+      </ColorModeProvider>
+    </Provider>
+  </StrictMode>,
+)
